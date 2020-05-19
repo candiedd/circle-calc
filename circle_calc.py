bcad79f0df4f449943a6c2c0978672407b06c1a5
candiedd <kjonaascooper@gmail.com>
2020-05-18 21:32:00 -0400

    Add files via upload

---
+++ circle_calc.py
@@ -0,0 +1,9 @@
+print("Circumference is calculated using 3.14159")	
+str_r = input("Radius: ")
+int_r = int(str_r)
+int_d = int_r*2
+str_d = str(int_d)
+int_c = 2*3.14159*int_r
+str_c = str(int_c)
+print("Your diameter is "+ str_d)
+print("Your circumference is " + str_c)
